7fcc849ff8d4df0befc3d5c00ff18067a3fb77c8
JonasBlaedel <119848473+JonasBlaedel@users.noreply.github.com>
2023-11-14 16:45:05 +0100

    card og mapping

---
+++ src/app/components/Quiz.jsx
@@ -0,0 +1,54 @@
+"use client";
+import React, { useState } from "react";
+
+function Quiz(props) {
+  // GET svarmuligheder fra Supabase
+  const [visible, setVisible] = useState(0);
+
+  // Filter the data based on props.step === 1
+  const filteredDataStepOne = props.data.filter((quest) => quest.step === 1);
+  const filteredDataStepTwo = props.data.filter((quest) => quest.step === 2);
+  const filteredDataStepThree = props.data.filter((quest) => quest.step === 3);
+  const filteredDataStepFour = props.data.filter((quest) => quest.step === 4);
+
+  const views = [
+    <section>
+      {filteredDataStepOne.map((quest) => (
+        <p>{quest.svarmulighed}</p>
+      ))}
+    </section>,
+    <section>
+      {filteredDataStepTwo.map((quest) => (
+        <p>{quest.svarmulighed}</p>
+      ))}
+    </section>,
+    <section>
+      {filteredDataStepThree.map((quest) => (
+        <p>{quest.svarmulighed}</p>
+      ))}
+    </section>,
+    <section>
+      {filteredDataStepFour.map((quest) => (
+        <p>{quest.svarmulighed}</p>
+      ))}
+    </section>,
+  ];
+
+  function setNextView() {
+    setVisible((oldValue) => {
+      if (oldValue === views.length - 1) {
+        return 0;
+      }
+      return oldValue + 1;
+    });
+  }
+
+  return (
+    <div>
+      {views[visible]}
+      <button onClick={setNextView}>Next view</button>
+    </div>
+  );
+}
+
+export default Quiz;
