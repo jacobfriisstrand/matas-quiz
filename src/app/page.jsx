ceb1b58635af4ec023b0af5e4448965c330cda85
Katjahkrogh <you@example.com>
2023-11-14 16:53:03 +0100

    data i page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,17 +4,22 @@ import Card from "./components/Card";
 
 export default async function page() {
   // GET svarmuligheder fra Supabase
-  const res = await fetch("https://largmqclsxkjplwlmtju.supabase.co/rest/v1/matas", {
-    method: "GET",
-    headers: {
-      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhcmdtcWNsc3hranBsd2xtdGp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE3MTUxMTYsImV4cCI6MTk5NzI5MTExNn0.nJMcytnEAVfVxQa6ZbZ4OGAF076k-1FVgNDmH32XjVs",
-    },
-  });
+  const res = await fetch(
+    "https://largmqclsxkjplwlmtju.supabase.co/rest/v1/matas",
+    {
+      method: "GET",
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhcmdtcWNsc3hranBsd2xtdGp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE3MTUxMTYsImV4cCI6MTk5NzI5MTExNn0.nJMcytnEAVfVxQa6ZbZ4OGAF076k-1FVgNDmH32XjVs",
+      },
+    }
+  );
   const data = await res.json();
 
   return (
     <main>
       <h1>Matas</h1>
+      <Quiz data={data} />
     </main>
   );
 }
