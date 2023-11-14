3cc3e890d0a9d12c9c0f8cf2fbe891c617975ae5
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2023-11-14 15:10:09 +0100

    color setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        matasBrown: {
+          200: "#C09B8C",
+        },
+        matasPink: {
+          700: "#c22869",
+        },
+        matasGreen: {
+          200: "#C6E7C9",
+        },
+        matasPurple: {
+          700: "#A094C4",
+        },
+        matasAqua: {
+          700: "#80CDC6",
+        },
+        matasWhite: {
+          900: "#FFFFFF",
+        },
+        matasBlue: {
+          900: "#002643",
+        },
+        matasSecondaryText: {
+          700: "#8C8C8C",
+        },
+        matasPrimaryText: {
+          900: "#000000",
+        },
+      },
+      plugins: [],
     },
   },
-  plugins: [],
-}
+};
