e79f859de31c8d5207ae2213677584ace7083401
Hannah2609 <hann7419@stud.kea.dk>
2023-11-16 20:20:25 +0100

    .

--- src/app/components/EndPage.jsx
+++ src/app/components/EndPage.jsx
@@ -4,9 +4,9 @@ function EndPage() {
   return (
     <div className="grid grid-cols-2 bg-matasPink-700">
       <Image src="/model.webp" width={500} height={500} alt="Model picture" />
-      <div>
+      <div className="grid place-content-center max-w-[70%]">
         <h2>DAGCREME</h2>
-        <h1>TIP</h1>
+        <h1 className="">TIP</h1>
         <p>
           Hvis du gerne vil gøre noget ekstra for din hud, så brug en serum
           under din dagcreme i din hudplejerutine
