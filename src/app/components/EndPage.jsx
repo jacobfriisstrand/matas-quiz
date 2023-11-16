35412a45b8ad421a28aec8318b9ae842ec31018f
Hannah2609 <hann7419@stud.kea.dk>
2023-11-16 20:34:17 +0100

    button

--- src/app/components/EndPage.jsx
+++ src/app/components/EndPage.jsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
+import styles from "@/app/components/endpage.module.css"
 
 function EndPage() {
   return (
     <div className="grid grid-cols-2 bg-matasPink-700">
       <Image src="/model.webp" width={500} height={500} alt="Model picture" />
-      <div className="grid place-content-center max-w-[70%]">
+      <div className={styles.textContainer} >
         <h2>DAGCREME</h2>
-        <h1 className="">TIP</h1>
+        <h1>TIP</h1>
         <p>
           Hvis du gerne vil gøre noget ekstra for din hud, så brug en serum
           under din dagcreme i din hudplejerutine
         </p>
-        <button>SE DIT RESULTAT</button>
+        <button className={styles.button}>SE DIT RESULTAT</button>
       </div>
     </div>
   );
