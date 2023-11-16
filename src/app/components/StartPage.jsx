b197fcaaa523b86e83728a251878511867daf183
Katjahkrogh <you@example.com>
2023-11-16 12:11:36 +0100

    startpage 1

--- src/app/components/StartPage.jsx
+++ src/app/components/StartPage.jsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
+
 function StartPage() {
   return (
-    <div>
-      <h1>This is start page</h1>
+    <div className="grid grid-cols-2 bg-matasPink-700">
+      <Image src="/model.webp" width={500} height={500} alt="Model picture" />
+      <div>
+        <p>FIND DIN FAVORIT</p>
+        <h1>Hvilken dagcreme passer til dig?</h1>
+        <button>Tag testen</button>
+        <h4>
+          <b>Optjen 5 point</b> hos club Matas
+        </h4>
+      </div>
     </div>
-  )
+  );
 }
 
-export default StartPage
+export default StartPage;
