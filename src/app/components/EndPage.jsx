18a1595a20c41ec1cdfc47799bf06a0340c5fd62
Hannah2609 <hann7419@stud.kea.dk>
2023-11-16 20:37:18 +0100

    .

--- src/app/components/EndPage.jsx
+++ src/app/components/EndPage.jsx
@@ -5,14 +5,14 @@ function EndPage() {
   return (
     <div className="grid grid-cols-2 bg-matasPink-700">
       <Image src="/model.webp" width={500} height={500} alt="Model picture" />
-      <div className={styles.textContainer} >
+      <div className={`${styles.textContainer}`}>
         <h2>DAGCREME</h2>
         <h1>TIP</h1>
         <p>
           Hvis du gerne vil gøre noget ekstra for din hud, så brug en serum
           under din dagcreme i din hudplejerutine
         </p>
-        <button className={styles.button}>SE DIT RESULTAT</button>
+        <button className={`${styles.button}`}>SE DIT RESULTAT</button>
       </div>
     </div>
   );
