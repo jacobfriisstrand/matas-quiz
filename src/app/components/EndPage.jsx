1edff309c84c336d2d56be7f1bdd163af185bfcb
Hannah2609 <hann7419@stud.kea.dk>
2023-11-19 21:11:00 +0100

    endpage done

--- src/app/components/EndPage.jsx
+++ src/app/components/EndPage.jsx
@@ -3,15 +3,15 @@ import styles from "@/app/components/endpage.module.css"
 
 function EndPage() {
   return (
-    <div className="grid grid-cols-2 bg-matasPink-700">
+    <div className="grid grid-cols-2 bg-matasPink-700 rounded-lg m-3">
       <Image src="/model.webp" width={500} height={500} alt="Model picture" />
       <div className={`${styles.textContainer}`}>
         <div>
-          <h2>DAGCREME</h2>
+          <h2 className="text-matasBlue-900 ">DAGCREME</h2>
           <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-matasBlue-900 font-serif font-semibold">
             TIP
           </h1>
-          <p className="max-w-[250px] mt-2	">
+          <p className="max-w-[300px] mt-2	">
             Hvis du gerne vil gøre noget ekstra for din hud, så brug en serum
             under din dagcreme i din hudplejerutine
           </p>
