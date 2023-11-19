c7c3b632e93a7279a2323084851636bebc54d674
Hannah2609 <hann7419@stud.kea.dk>
2023-11-19 22:49:19 +0100

    center

--- src/app/components/AnalyzingPage.jsx
+++ src/app/components/AnalyzingPage.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 function AnalyzingPage() {
   return (
-    <div className='bg-matasBeige-100 rounded-lg m-3 h-500"'>
+    <div className='bg-matasBeige-100 rounded-lg m-3 h-500 text-center"'>
       <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-matasPrimaryText-900 font-serif font-semibold">
         Beregner dine svar
       </h1>
+      <h2 className="text-matasBlue-900 ">FINDER DIN FAVORIT</h2>
     </div>
   );
 }
