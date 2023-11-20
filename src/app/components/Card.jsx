f5071df5a6d4b74b71120c009594d2e9767bd85d
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2023-11-20 15:17:54 +0100

    page styling and fetching done

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 
 function Card({ promotion, src, brand, title, price }) {
   return (
-    <article className="p-4 bg-white rounded-md flex gap-4 flex-col flex-auto">
+    <article className="space-y-5 bg-white rounded-lg p-4 grid items-stretch">
       {promotion && <p className=" bg-red-600 w-fit p-1 rounded-md uppercase text-xs text-white ">{promotion} </p>}
-      <div className="flex justify-center items-center h-40">
-        <div className="w-32  ">
-          <Image src={`/${src}`} className=" " width={400} height={400} alt="product image" />
+      <div className="">
+        <div className="">
+          <Image src={`${src}`} className="aspect-square object-contain mx-auto" width={300} height={300} alt="product image" />
         </div>
       </div>
       <div>
         <p className="font-semibold">{brand}</p>
         <p>{title}</p>
       </div>
-      <h3 className="text-lg">{price} kr.</h3>
+      <h3 className="text-lg">{price}</h3>
       <div className="flex gap-2 flex-row w-full">
         <button className="bg-cyan-950 text-white p-2 w-full rounded-md">Læg i kurv</button>
         <button className="bg-neutral-400 p-2">&#9825;</button>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,66 +2,37 @@ import Quiz from "./components/Quiz";
 import React from "react";
 import Card from "./components/Card";
 import EndPage from "./components/EndPage";
+import { promises as fs } from "fs";
 
 // Udkommenterring skal fjernes, hvis database skal opdateres.
 export const dynamic = "force-dynamic";
 
 export default async function page() {
   // GET svarmuligheder fra Supabase
-  const res = await fetch(
-    "https://largmqclsxkjplwlmtju.supabase.co/rest/v1/matas",
-    {
-      method: "GET",
-      headers: {
-        apikey:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhcmdtcWNsc3hranBsd2xtdGp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE3MTUxMTYsImV4cCI6MTk5NzI5MTExNn0.nJMcytnEAVfVxQa6ZbZ4OGAF076k-1FVgNDmH32XjVs",
-      },
-    }
-  );
+  const res = await fetch("https://largmqclsxkjplwlmtju.supabase.co/rest/v1/matas", {
+    method: "GET",
+    headers: {
+      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhcmdtcWNsc3hranBsd2xtdGp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE3MTUxMTYsImV4cCI6MTk5NzI5MTExNn0.nJMcytnEAVfVxQa6ZbZ4OGAF076k-1FVgNDmH32XjVs",
+    },
+  });
   const data = await res.json();
-  console.log(data);
+
+  const file = await fs.readFile(process.cwd() + "/public/produkt.json", "utf8");
+  const productData = JSON.parse(file);
+
+  console.log(productData);
 
   return (
-    <main>
-      <h1>Matas</h1>
-      <section className="lg:flex lg:flex-wrap -mx-2 grid grid-cols-2">
-        <div className="lg:w-1/3 px-2 flex items-end">
-          <Card
-            promotion="Spar 50%"
-            src="dagcreme.avif"
-            brand="matas"
-            title="Dagcreme"
-            price="599"
-          />
-        </div>
-        <div className=" lg:w-1/3 px-2 flex items-end">
-          <Card
-            src="dagcreme.avif"
-            brand="matas"
-            title="Dagcreme"
-            price="599"
-          />
-        </div>
-        <div className="lg:w-1/3 px-2 row-start-3 lg:row-start-1 flex items-end">
-          <Card
-            promotion="200kr for club matas"
-            src="dagcreme.avif"
-            brand="matas"
-            title="Dagcreme"
-            price="599"
-          />
-        </div>
-        <div className=" lg:col-span-1 col-span-2 md:w-full lg:w-2/3  px-2">
+    <main className="bg-matasEarth-500">
+      <section className="grid md:grid-cols-3 lg:grid-cols-4 gap-10">
+        <div className="col-span-2 row-start-2">
           <Quiz data={data} />
         </div>
-        <div className="lg:w-1/3 px-2 flex items-end">
-          <Card
-            src="dagcreme.avif"
-            brand="matas"
-            title="Dagcreme"
-            price="599"
-          />
-        </div>
+        {productData.map((product) => (
+          <div key={product.id}>
+            <Card src={product.image} brand={product.brand} title={product.name} price={product.price} />
+          </div>
+        ))}
       </section>
     </main>
   );
