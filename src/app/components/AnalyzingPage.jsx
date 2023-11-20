f5fc3408654bf92014508742e742bded38821d51
Hannah2609 <hann7419@stud.kea.dk>
2023-11-20 15:08:01 +0100

    .

--- src/app/components/AnalyzingPage.jsx
+++ src/app/components/AnalyzingPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function AnalyzingPage() {
   return (
-    <div></div>
-  )
+    <div className='bg-matasBeige-100 rounded-lg m-3 grid place-items-center "'>
+      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-matasPrimaryText-900 font-serif font-semibold">
+        Beregner dine svar
+      </h1>
+      <h2 className="text-matasBlue-900 text-xs sm:text-base lg:text-xl ">
+        FINDER DIN FAVORIT
+      </h2>
+    </div>
+  );
 }
 
-export default AnalyzingPage
+export default AnalyzingPage;
