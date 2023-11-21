dc7b11554a1b4d25454c37ecd337649e680da677
Hannah2609 <hann7419@stud.kea.dk>
2023-11-21 14:22:49 +0100

    doneeee

--- src/app/components/Quiz.jsx
+++ src/app/components/Quiz.jsx
@@ -54,13 +54,22 @@ function Quiz(props) {
         .filter((item) => item.step === step && item.questions !== null)
         .map((item) => (
           <div key={item.id}>
-            <Image className="aspect-video w-full max-h-52 object-center object-cover" src={`/${item.hero}`} width={600} height={600} alt="Model picture" />
-            <h3 className="font-serif font-semibold text-2xl mt-5">{item.questions}</h3>
+            <Image
+              className="aspect-video w-full max-h-52 object-center object-cover"
+              src={`/${item.hero}`}
+              width={600}
+              height={600}
+              alt="Model picture"
+            />
+            <h3 className="font-serif font-semibold text-2xl mt-5">
+              {item.questions}
+            </h3>
             <p>{item.options}</p>
           </div>
         ))}
-      {step === 0 && <StartPage setStep={setStep}/>}
+      {step === 0 && <StartPage setStep={setStep} />}
       {step === 5 && <AnalyzingPage setStep={setStep} />}
+      {step === 6 && <EndPage setStep={setStep} />}
       <div className="flex gap-x-10 flex-wrap my-10">
         {props.data
           .filter((item) => item.step === step && item.svarmulighed !== null)
@@ -85,14 +94,17 @@ function Quiz(props) {
                   value={item.svarmulighed}
                 />
               )}
-              <label className={`${styles.inputChildren} p-5 cursor-pointer peer-checked:text-white peer-hover:text-white`} htmlFor={item.id}>
+              <label
+                className={`${styles.inputChildren} p-5 cursor-pointer peer-checked:text-white peer-hover:text-white`}
+                htmlFor={item.id}
+              >
                 {item.svarmulighed}
               </label>
             </div>
           ))}
       </div>
       <div className="flex justify-center w-full gap-20">
-        {step > 0 && step !== 5 && (
+        {step !== 0 && step !== 5 && step !== 6 && (
           <button
             className="flex  place-items-center  gap-2 py-2 px-7 rounded-full hover:bg-transparent hover:ring-inset ring-2 ring-matasBlue-900 hover:text-matasBlue-900 hover:border-matasBlue-900 bg-matasBlue-900 text-matasWhite-900"
             onClick={() => {
@@ -100,13 +112,20 @@ function Quiz(props) {
               setStep((prevStep) => prevStep - 1);
             }}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-caret-left-fill"
+              viewBox="0 0 16 16"
+            >
               <path d="M3.86 8.753l5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
             </svg>{" "}
             Forrige
           </button>
         )}
-        {step > 0 && step !== 5 && (
+        {step > 0 && step !== 5 && step !== 6 && (
           <button
             className="flex  place-items-center  gap-2 py-2 px-7 rounded-full hover:bg-transparent hover:ring-inset ring-2 ring-matasBlue-900 hover:text-matasBlue-900 hover:border-matasBlue-900 bg-matasBlue-900 text-matasWhite-900"
             onClick={() => {
@@ -122,7 +141,14 @@ function Quiz(props) {
             }}
           >
             Næste{" "}
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-caret-right-fill"
+              viewBox="0 0 16 16"
+            >
               <path d="M12.14 8.753l-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
             </svg>
           </button>
