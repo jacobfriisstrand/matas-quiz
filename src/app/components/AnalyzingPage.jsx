393509f0168869739c8380a2e05e054bd3a8d625
Hannah2609 <hann7419@stud.kea.dk>
2023-11-21 14:12:48 +0100

    ani

--- src/app/components/AnalyzingPage.jsx
+++ src/app/components/AnalyzingPage.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Animation from "./Animaton";
 
-function AnalyzingPage() {
+function AnalyzingPage({ setStep }) {
   return (
-    <div></div>
-  )
+    <div className="bg-matasBrown-200 rounded-lg m-3 grid place-items-center ">
+      <Animation setStep={setStep} />
+      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-matasPrimaryText-900 font-serif font-semibold">
+        Beregner dine svar
+      </h1>
+      <h2 className="text-matasBlue-900 text-xs sm:text-base lg:text-xl ">
+        FINDER DIN FAVORIT
+      </h2>
+    </div>
+  );
 }
 
-export default AnalyzingPage
+export default AnalyzingPage;
