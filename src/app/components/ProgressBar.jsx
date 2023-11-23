e9c45a5a7b2f1ab6759d820e4c1467fdc6d3caa6
Katjahkrogh <you@example.com>
2023-11-23 11:36:35 +0100

    progressbar

--- src/app/components/ProgressBar.jsx
+++ src/app/components/ProgressBar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
-function ProgressBar() {
-  return (
-    <div>ProgressBar</div>
-  )
-}
-
-export default ProgressBar
+import { Steps } from "antd";
+const items = [
+  {
+    title: "1",
+  },
+  {
+    title: "2",
+  },
+  {
+    title: "3",
+  },
+  {
+    title: "4",
+  },
+];
+const ProgressBar = ({ step }) => (
+  <>
+    <Steps
+      current={step - 1}
+      size="small"
+      labelPlacement="vertical"
+      items={items}
+    />
+  </>
+);
+export default ProgressBar;
