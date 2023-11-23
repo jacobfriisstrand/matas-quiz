688c3e057c8a38512e5f90a1550558f30ea901ed
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2023-11-23 10:11:28 +0100

    jump fix

--- src/app/components/EndPage.jsx
+++ src/app/components/EndPage.jsx
@@ -4,25 +4,12 @@ import styles from "@/app/components/endpage.module.css";
 function EndPage() {
   return (
     <div className="grid grid-cols-2 bg-matasPink-700 rounded-md overflow-hidden gap-4 min-h-full ">
-      <Image
-        src="/model.webp"
-        width={500}
-        height={800}
-        alt="Model picture"
-        className="aspect-[9/13.6] h-full object-cover"
-      />
+      <Image src="/model.webp" width={500} height={100} alt="Model picture" className="h-full object-cover" />
       <div className={`${styles.textContainer}`}>
         <div>
-          <h2 className="text-matasBlue-900 text-xs sm:text-xl lg:text-base">
-            DAGCREME
-          </h2>
-          <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl text-matasBlue-900 font-serif font-semibold mt-2 ">
-            TIP
-          </h1>
-          <p className="max-w-[300px] mt-2 text-xs sm:text-base lg:text-base	">
-            Hvis du gerne vil gøre noget ekstra for din hud, så brug en serum
-            under din dagcreme i din hudplejerutine
-          </p>
+          <h2 className="text-matasBlue-900 text-xs sm:text-xl lg:text-base">DAGCREME</h2>
+          <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl text-matasBlue-900 font-serif font-semibold mt-2 ">TIP</h1>
+          <p className="max-w-[300px] mt-2 text-xs sm:text-base lg:text-base	">Hvis du gerne vil gøre noget ekstra for din hud, så brug en serum under din dagcreme i din hudplejerutine</p>
           <button className="p-2 sm:p-3 text-matasWhite-900 bg-matasBlue-900 rounded-md mt-10 w-full">
             <p className="text-xs sm:text-base lg:text-base">SE DIT RESULTAT</p>
           </button>
