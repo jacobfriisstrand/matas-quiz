40a417dd7ff640479a30db2e3cdb522b7ac97c15
Hannah2609 <hann7419@stud.kea.dk>
2023-11-23 15:21:46 +0100

    resultater

--- src/app/components/Wrapper.jsx
+++ src/app/components/Wrapper.jsx
@@ -3,6 +3,7 @@ import { useState, useRef, useEffect } from "react";
 import Quiz from "./Quiz";
 import Card from "./Card";
 import styles from "./Wrapper.module.css";
+import Image from "next/image";
 import { DM_Serif_Text } from "next/font/google";
 
 const dmSerif = DM_Serif_Text({
@@ -12,7 +13,7 @@ const dmSerif = DM_Serif_Text({
 });
 
 function Wrapper({ productData, data }) {
-  const [step, setStep] = useState(7);
+  const [step, setStep] = useState(0);
 
   useEffect(() => {
     if (step === 7) {
@@ -27,65 +28,65 @@ function Wrapper({ productData, data }) {
   });
 
   return (
-    <section>
+    <section className="mx-6">
       {step === 7 ? (
         <>
-          <section className=" bg-matasBeige-200 rounded-md mx-2">
-            <div className="flex justify-between px-4 py-10">
+          <section className=" bg-matasBeige-200 rounded-md grid grid-cols-1 md:grid-cols-3 overflow-hidden">
+            <div className="col-span-2 p-4 md:p-8 space-y-4">
               <div>
-                <h3 className={`${dmSerif.className} font-semibold text-xl md:text-2xl text-matasBlue-900`}>
-                  Formål
+                <p className="text-matasBlue-900 text-xs sm:text-sm lg:text-base">
+                  DAGCREME 
+                </p>
+                <h3
+                  className={`${dmSerif.className} text-2xl md:text-3xl text-matasBlue-900 font-semibold`}
+                >
+                  Dine svar
                 </h3>{" "}
-                <div className="flex flex-wrap gap-2">
-                  {ref.current.purpose.map((object, index) => (
-                    <p
-                      className="bg-matasBlue-900 text-matasWhite-900 p-2 rounded-full h-fit text-xs max-w-max mb-1"
-                      key={index}
-                    >
-                      {object}
-                    </p>
-                  ))}
-                </div>
               </div>
-              <div>
-                <h3 className={`${dmSerif.className} font-semibold text-xl md:text-2xl text-matasBlue-900`}>
-                  Præferencer
-                </h3>
-                <p className="bg-matasBlue-900 text-matasWhite-900 p-2 rounded-full h-fit text-xs max-w-max mb-1">
+              <div className=" flex flex-wrap gap-2">
+                {ref.current.purpose.map((object, index) => (
+                  <p
+                    className="bg-matasBlue-900 text-matasWhite-900 py-2 px-4 rounded-full h-fit text-xs max-w-max mb-1"
+                    key={index}
+                  >
+                    {object}
+                  </p>
+                ))}
+
+                <p className="bg-matasBlue-900 text-matasWhite-900 py-2 px-4 rounded-full h-fit text-xs max-w-max mb-1">
                   {ref.current.important}
                 </p>
-              </div>
-              <div>
-                <h3 className={`${dmSerif.className} font-semibold text-xl md:text-2xl text-matasBlue-900`}>
-                  Alder
-                </h3>
-                <p className="bg-matasBlue-900 text-matasWhite-900 p-2 rounded-full h-fit text-xs max-w-max mb-1">
+
+                <p className="bg-matasBlue-900 text-matasWhite-900 py-2 px-4 rounded-full h-fit text-xs max-w-max mb-1">
                   {ref.current.age}
                 </p>
-              </div>
-              <div>
-                <h3 className={`${dmSerif.className} font-semibold text-xl md:text-2xl text-matasBlue-900`}>
-                  Hudtype
-                </h3>
-                <p className="bg-matasBlue-900 text-matasWhite-900 p-2 rounded-full h-fit text-xs max-w-max mb-1">
+
+                <p className="bg-matasBlue-900 text-matasWhite-900 py-2 px-4 rounded-full h-fit text-xs max-w-max mb-1">
                   {ref.current.skinType}
                 </p>
-              </div>
-            </div>{" "}
-            <div className="flex flex-col items-end">
-              <h4 className="text-matasBlue-900 text-xs sm:text-base lg:text-base mr-4 ">
+              </div>{" "}
+              <h4 className="text-matasBlue-900 text-xs sm:text-base lg:text-base pt-2 md:pt-4">
                 <b>Optjen 5 point</b> hos club Matas
               </h4>
-              <button className="py-2 px-8  text-matasBlue-900 ring-2 ring-matasBlue-900 rounded-md m-4 ">
+              <button className="py-2 px-20  text-matasBlue-900 ring-2 ring-matasBlue-900 rounded-md w-full md:w-fit">
                 <p className="text-xs sm:text-base lg:text-base h-fit">
-                  GEM DINE SVAR
+                  GEM SVAR
                 </p>
               </button>
             </div>
+            <Image
+              className="aspect-video w-full min-h-full object-center object-cover row-start-1 md:col-start-3"
+              src="/model.webp"
+              width={600}
+              height={300}
+              alt="Model picture"
+            />
           </section>
 
-          <h2 className={`${dmSerif.className} font-serif font-semibold mt-5 text-2xl sm:text-2xl md:text-3xl lg:text-3xl text-matasBlue-900`}>
-            Dine Resultater{" "}
+          <h2
+            className={`${dmSerif.className} font-serif font-semibold mt-8 text-2xl sm:text-2xl md:text-3xl lg:text-3xl text-matasBlue-900 mb-4`}
+          >
+            Resultater{" "}
           </h2>
           <section className={styles.grid}>
             {productData.slice(0, 3).map((product) => (
