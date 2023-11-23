9b4b8fcc8bbd4a414e0bc0c39d536050bf903451
JonasBlaedel <119848473+JonasBlaedel@users.noreply.github.com>
2023-11-23 11:42:21 +0100

    wrapper

--- src/app/components/EndPage.jsx
+++ src/app/components/EndPage.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "@/app/components/endpage.module.css";
 
-function EndPage() {
+function EndPage({ setStep }) {
   return (
     <div className="grid grid-cols-2 bg-matasPink-700 rounded-md overflow-hidden min-h-full ">
       <Image src="/model.webp" width={500} height={100} alt="Model picture" className="h-full object-cover" />
@@ -10,7 +10,13 @@ function EndPage() {
           <h2 className="text-matasBlue-900 text-xs sm:text-xl lg:text-base">DAGCREME</h2>
           <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl text-matasBlue-900 font-serif font-semibold mt-2 ">TIP</h1>
           <p className="max-w-[300px] mt-2 text-xs sm:text-base lg:text-base	">Hvis du gerne vil gøre noget ekstra for din hud, så brug en serum under din dagcreme i din hudplejerutine</p>
-          <button className="p-2 sm:p-3 text-matasWhite-900 bg-matasBlue-900 rounded-md mt-10 w-full">
+          <button
+            onClick={() => {
+              setStep((prevStep) => prevStep + 1);
+              return;
+            }}
+            className="p-2 sm:p-3 text-matasWhite-900 bg-matasBlue-900 rounded-md mt-10 w-full"
+          >
             <p className="text-xs sm:text-base lg:text-base">SE DIT RESULTAT</p>
           </button>
         </div>
