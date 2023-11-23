90af103ccc088e64dc97bcb3f383822f272ded4d
Katjahkrogh <you@example.com>
2023-11-23 12:25:04 +0100

    styling

--- src/app/components/ProgressBar.jsx
+++ src/app/components/ProgressBar.jsx
@@ -1,26 +1,37 @@
-import { Steps } from "antd";
+import { Steps, ConfigProvider } from "antd";
 const items = [
   {
-    title: "",
+    title: "Formål",
   },
   {
-    title: "",
+    title: "Præferencer",
   },
   {
-    title: "",
+    title: "Alder",
   },
   {
-    title: "",
+    title: "Hudtype",
   },
 ];
 const ProgressBar = ({ step }) => (
   <>
-    <Steps
-      current={step - 1}
-      size="small"
-      labelPlacement="vertical"
-      items={items}
-    />
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#002643",
+          colorText: "#002643",
+        },
+      }}
+    >
+      <div className="my-5 text-white">
+        <Steps
+          current={step - 1}
+          size="small"
+          labelPlacement="vertical"
+          items={items}
+        />
+      </div>
+    </ConfigProvider>
   </>
 );
 export default ProgressBar;
