4e99d3825325b3f2e4770ae1914ceb38d3f132be
Hannah2609 <hann7419@stud.kea.dk>
2023-11-23 10:01:46 +0100

    fonte

--- src/app/components/EndPage.jsx
+++ src/app/components/EndPage.jsx
@@ -13,18 +13,18 @@ function EndPage() {
       />
       <div className={`${styles.textContainer}`}>
         <div>
-          <h2 className="text-matasBlue-900 text-xs sm:text-xl lg:text-xl">
+          <h2 className="text-matasBlue-900 text-xs sm:text-xl lg:text-base">
             DAGCREME
           </h2>
-          <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-matasBlue-900 font-serif font-semibold mt-2">
+          <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl text-matasBlue-900 font-serif font-semibold mt-2 ">
             TIP
           </h1>
-          <p className="max-w-[300px] mt-2	">
+          <p className="max-w-[300px] mt-2 text-xs sm:text-base lg:text-base	">
             Hvis du gerne vil gøre noget ekstra for din hud, så brug en serum
             under din dagcreme i din hudplejerutine
           </p>
           <button className="p-2 sm:p-3 text-matasWhite-900 bg-matasBlue-900 rounded-md mt-10 w-full">
-            <p className="text-xs sm:text-xl lg:text-xl">SE DIT RESULTAT</p>
+            <p className="text-xs sm:text-base lg:text-base">SE DIT RESULTAT</p>
           </button>
         </div>
       </div>
--- src/app/components/StartPage.jsx
+++ src/app/components/StartPage.jsx
@@ -6,17 +6,17 @@ export default function StartPage({ setStep }) {
     <div className="grid grid-cols-2 bg-matasPink-700 rounded-md overflow-hidden min-h-full ">
       <Image src="/model.webp" width={500} height={100} alt="Model picture" className="h-full object-cover" />
       <div className="gap-3 px-4 flex flex-col justify-center align-center text-center mx-auto md:gap-5">
-        <p className="text-matasBlue-900 text-xs sm:text-xl lg:text-xl">FIND DIN FAVORIT</p>
-        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-matasBlue-900 font-serif font-semibold">Hvilken dagcreme passer til dig?</h1>
+        <p className="text-matasBlue-900 text-xs sm:text-xl lg:text-base">FIND DIN FAVORIT</p>
+        <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl text-matasBlue-900 font-serif font-semibold">Hvilken dagcreme passer til dig?</h1>
         <button
           onClick={() => {
             setStep((prevStep) => prevStep + 1);
           }}
           className="p-2 sm:p-3 text-matasWhite-900 bg-matasBlue-900 rounded-md"
         >
-          <p className="text-xs sm:text-xl lg:text-xl">TAG TESTEN</p>
+          <p className="text-xs sm:text-base lg:text-base">TAG TESTEN</p>
         </button>
-        <h4 className="text-matasBlue-900 text-xs sm:text-xl lg:text-xl">
+        <h4 className="text-matasBlue-900 text-xs sm:text-base lg:text-base">
           <b>Optjen 5 point</b> hos club Matas
         </h4>
       </div>
